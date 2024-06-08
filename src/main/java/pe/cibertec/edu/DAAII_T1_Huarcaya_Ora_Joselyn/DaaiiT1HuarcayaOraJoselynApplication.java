59bb8c187a3060db9ea775f535dbe7a3deeeabc5
JossyRos <huarcayajoselyn1@gmail.com>
2024-06-08 02:20:40 -0500

    pregunta 1

---
+++ src/main/java/pe/cibertec/edu/DAAII_T1_Huarcaya_Ora_Joselyn/DaaiiT1HuarcayaOraJoselynApplication.java
@@ -0,0 +1,13 @@
+package pe.cibertec.edu.DAAII_T1_Huarcaya_Ora_Joselyn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaaiiT1HuarcayaOraJoselynApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaaiiT1HuarcayaOraJoselynApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/cibertec/edu/DAAII_T1_Huarcaya_Ora_Joselyn/DaaiiT1HuarcayaOraJoselynApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.cibertec.edu.DAAII_T1_Huarcaya_Ora_Joselyn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaaiiT1HuarcayaOraJoselynApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
